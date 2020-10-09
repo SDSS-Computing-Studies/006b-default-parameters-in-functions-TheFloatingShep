40bee499371c3d72a6bc1ad04c5d048d0e328487
Kenji MacMartin <47537573+TheFloatingShep@users.noreply.github.com>
2020-10-09 12:17:31 -0700

    fortnite lol

--- assignment.py
+++ assignment.py
@@ -2,18 +2,52 @@
 
 import math
 
-def tempConversion():
+def tempConversion(x,unit):
+    if unit == "F":
+        return float(round(((x - 32) * (5/9)), 1))
+    else:
+        return float(round(((x * (9/5)) + 32), 1))
 
+def factorPair(x,y):
+    lis = [y]
+    lis.append((x/y))
+    lis.sort()
+    return lis
 
-def factorPair():
-
-
-
-def cosineLaw():
-
+def cosineLaw(a,b,c,d):
+    return "jeff"
 def toRadians():
-
+    return "jeff"
 def solution():
-
+    return "jeff"
 def quadratic():
+    return "jeff"
 
+a = int(input("Enter side A\n"))
+b = int(input("Enter side B\n"))
+c = int(input("Enter side C\n"))
+if a >= b and a >= c:
+    C = a
+    B = b
+    A = c
+if b >= a and b >= c:
+    C = b
+    B = a
+    A = c
+if c >= b and c >= a:
+    C = c
+    B = b
+    A = a
+if A > B:
+    B2 = A
+    A2 = B
+else:
+    B2 = B
+    A2 = A
+An = str(A2)
+Bn = str(B2)
+Cn = str(C)
+if (A2**2) + (B2**2) == (C**2):
+    print(An + "," + Bn + "," + Cn + " form a Pythagorean triple")
+else:
+    print(An + "," + Bn + "," + Cn + " do not form a Pythagorean triple")
--- extension.py
+++ extension.py
@@ -26,7 +26,84 @@ input parameter. It's return value is either:
 "O" (O is a winner)
 
 """
-
+"""
 def winner(game):
     # input: list that contains 10 elements
     # output: either 0, "X", "O" to indicate who the winner is.
+
+                                                                    I make a whole game instead have fun
+
+
+TIC TAC TOE
+x.o
+.x.
+.xo
+Input number for where you want your piece
+                123
+                456
+                789
+"""
+board = ["empty",".",".",".",".",".",".",".",".","."]
+
+def dis():
+    print(board[1] + board[2] + board[3] + "\n" + board[4] + board[5] + board[6] + "\n" + board[7] + board[8] + board[9])
+
+def xTurn():
+    x = int(input("X turn: "))
+    if  x > 0 and x < 10 and board[x] == ".":
+        board[x] = "X"
+        dis()
+        if check("X") == False:
+            yTurn()
+    else:
+        print("Invalid space")
+        xTurn()
+
+def yTurn():
+    y = int(input("Y turn: "))
+    if  y > 0 and y < 10 and board[y] == ".":
+        board[y] = "O"
+        dis()
+        if check("O") == False:
+            xTurn()
+    else:
+        print("Invalid space")
+        yTurn()
+
+def win(winner):
+    print("Game over")
+    print(winner + " won the game!")
+
+def check(n):
+    if board[1] == n and board[2] == n and board[3] == n:
+        win(n)
+        return True
+    elif board[4] == n and board[5] == n and board[6] == n:
+        win(n)
+        return True
+    elif board[7] == n and board[8] == n and board[9] == n:
+        win(n)
+        return True
+    elif board[1] == n and board[4] == n and board[7] == n:
+        win(n)
+        return True
+    elif board[2] == n and board[5] == n and board[8] == n:
+        win(n)
+        return True
+    elif board[3] == n and board[6] == n and board[9] == n:
+        win(n)
+        return True
+    elif board[1] == n and board[5] == n and board[9] == n:
+        win(n)
+        return True
+    elif board[3] == n and board[5] == n and board[7] == n:
+        win(n)
+        return True
+    elif "." not in board:
+        win("Nobody")
+        return True
+    else:
+        return False
+
+dis()
+xTurn()
