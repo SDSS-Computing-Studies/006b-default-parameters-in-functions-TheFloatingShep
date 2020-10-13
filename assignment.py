aae524656851f4fec804ceaf7d98cad407d8883c
Kenji MacMartin <47537573+TheFloatingShep@users.noreply.github.com>
2020-10-13 11:42:05 -0700

    IT WORKS YESSS

--- assignment.py
+++ assignment.py
@@ -14,22 +14,30 @@ def factorPair(x,y):
     lis.sort()
     return lis
 
-def cosineLaw(a,b,C,oppositeSide):
+def cosineLaw(A,B,C,oppositeSide=True):
+    if A >= B:
+        b = A
+        a = B
+    else:
+        a = A
+        b = B
     if oppositeSide == True:
         c = math.sqrt(a**2 + b**2 - 2 * a * b * math.cos(toRadians(C)))
         return c
     elif oppositeSide == False:
-        n = quadratic((b**2) , (2 * a * math.cos(toRadians(C))) , (a**2)-(C**2))
+        n = quadratic( 1 , (-2 * a * math.cos(toRadians(C))) , (a**2)-(b**2))
         return solution(n)
+
 def toRadians(x):
     return (x * math.pi) / 180
+
 def solution(n):
     if n[0] < 0:
         return n[1]
     else:
         return n
+
 def quadratic(a,b,c):
     sols = [((-b + (math.sqrt((b**2) - 4 * a * c))) / (2 * a)),((-b - (math.sqrt((b**2) - 4 * a * c))) / (2 * a))]
     sols.sort()
     return sols
-print(cosineLaw(3,2,25,False))
